48e64c61d223ac943eb0e2e1385ad2cf031b7288
Arjan Jassal <arjan.jassal.27@gmail.com>
2016-03-26 01:40:54 -0700

    mooooar

--- public/user/themes/cosmicdropper/js/src/index.js
+++ public/user/themes/cosmicdropper/js/src/index.js
@@ -1,3 +1,4 @@
+const body = document.querySelector('body');
 const logo = document.getElementById('logo');
 const logoPaths = Array.from(logo.querySelectorAll('path'));
 
@@ -6,6 +7,9 @@ document.addEventListener('DOMContentLoaded', () => {
 	img.src = '/images/home-bg.jpg';
 	img.onload = () => {
 		img = null;
+
+		body.classList.add('show');
+
 		setTimeout(() => {
 				logoPaths.forEach((p) => p.classList.add('done'));
 			}, 100);
