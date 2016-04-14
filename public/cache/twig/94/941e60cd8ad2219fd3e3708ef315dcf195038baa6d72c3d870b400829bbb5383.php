9c593c0489575d9e4e636f2163d87d0e65fb542a
Arjan Jassal <arjan.jassal.27@gmail.com>
2016-04-14 15:09:44 -0700

    add curve bottom and curve shadow to home hero

--- public/cache/twig/94/941e60cd8ad2219fd3e3708ef315dcf195038baa6d72c3d870b400829bbb5383.php
+++ public/cache/twig/94/941e60cd8ad2219fd3e3708ef315dcf195038baa6d72c3d870b400829bbb5383.php
@@ -17,10 +17,33 @@ class __TwigTemplate_a24a3dc7a33f8aa66175a77b74399b48364ddb21556d0be7a5acb6b0f7f
     {
         // line 1
         echo "<section class=\"home-hero\">
-\t<h1>";
-        // line 2
-        echo $this->getAttribute((isset($context["page"]) ? $context["page"] : null), "title", array());
-        echo "</h1>
+\t<div class=\"u-wrapper\">
+\t\t<div class=\"home-hero__img\">
+\t\t\t<img width=\"400\" height=\"540\" class=\"home-hero__img-img\" src=\"/images/d-double-e-home.svg\" alt=\"\">
+\t\t\t<div class=\"home-hero__svgs\">
+\t\t\t\t";
+        // line 6
+        $this->loadTemplate("./partials/logo.html.twig", "modular/hero.html.twig", 6)->display($context);
+        // line 7
+        echo "\t\t\t\t";
+        $this->loadTemplate("./partials/logo.html.twig", "modular/hero.html.twig", 7)->display($context);
+        // line 8
+        echo "\t\t\t</div>
+\t\t</div>
+\t</div>
+
+\t<svg class=\"home-hero__curve\" xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" x=\"0\" y=\"0\" width=\"1920\" height=\"331.1\" viewBox=\"0 0 1920 331.1\" enable-background=\"new 0 0 1920 331.144\" xml:space=\"preserve\"><path d=\"M261.5 199c199.5 0 336 129.1 564 132.1 228 3 355.5-108.2 697.5-109.7 153.9-0.7 290.8 28.2 397 62.3V0H0v267.7C84.9 230.4 180.4 199 261.5 199z\"/></svg>
+
+\t<img class=\"home-hero__curve-shadow\" src=\"/images/paint-shadow.png\" alt=\"\">
+
+\t";
+        // line 19
+        echo "\t
+\t";
+        // line 20
+        $this->loadTemplate("./partials/paint-drips.html.twig", "modular/hero.html.twig", 20)->display($context);
+        // line 21
+        echo "
 </section>";
     }
 
@@ -36,9 +59,28 @@ class __TwigTemplate_a24a3dc7a33f8aa66175a77b74399b48364ddb21556d0be7a5acb6b0f7f
 
     public function getDebugInfo()
     {
-        return array (  22 => 2,  19 => 1,);
+        return array (  46 => 21,  44 => 20,  41 => 19,  31 => 8,  28 => 7,  26 => 6,  19 => 1,);
     }
 }
 /* <section class="home-hero">*/
-/* 	<h1>{{ page.title }}</h1>*/
+/* 	<div class="u-wrapper">*/
+/* 		<div class="home-hero__img">*/
+/* 			<img width="400" height="540" class="home-hero__img-img" src="/images/d-double-e-home.svg" alt="">*/
+/* 			<div class="home-hero__svgs">*/
+/* 				{% include "./partials/logo.html.twig" %}*/
+/* 				{% include "./partials/logo.html.twig" %}*/
+/* 			</div>*/
+/* 		</div>*/
+/* 	</div>*/
+/* */
+/* 	<svg class="home-hero__curve" xmlns="http://www.w3.org/2000/svg" version="1.1" x="0" y="0" width="1920" height="331.1" viewBox="0 0 1920 331.1" enable-background="new 0 0 1920 331.144" xml:space="preserve"><path d="M261.5 199c199.5 0 336 129.1 564 132.1 228 3 355.5-108.2 697.5-109.7 153.9-0.7 290.8 28.2 397 62.3V0H0v267.7C84.9 230.4 180.4 199 261.5 199z"/></svg>*/
+/* */
+/* 	<img class="home-hero__curve-shadow" src="/images/paint-shadow.png" alt="">*/
+/* */
+/* 	{# <svg class="home-hero__bottom"  version="1.1" x="0" y="0" width="2372" height="190.1" viewBox="0 0 2372 190.1" enable-background="new 0 0 2372 190.087" xml:space="preserve"><path d="M0 190.1C0 190.1 287.1 0 486.5 0c199.5 0 336 129.1 564 132.1 228 3 355.5-108.2 697.5-109.7S2348 165.1 2372 190.1C2307.5 190.1 0 190.1 0 190.1z"/>*/
+/* 	</svg>*/
+/*  #}*/
+/* 	*/
+/* 	{% include './partials/paint-drips.html.twig' %}*/
+/* */
 /* </section>*/
