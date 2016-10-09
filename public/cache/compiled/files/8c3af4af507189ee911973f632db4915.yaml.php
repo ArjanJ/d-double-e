6df53d2fe1a391d897d98a6589ceb6560bfadea7
Arjan Jassal <arjan.jassal.27@gmail.com>
2016-10-08 21:03:02 -0700

    reinstall email plugin

--- public/cache/compiled/files/8c3af4af507189ee911973f632db4915.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/Users/Arjan/GitHub/d-double-e/public/user/config/security.yaml',
-    'modified' => 1457675938,
-    'data' => [
-        'salt' => 'BGmi7WvU43OISJ'
-    ]
-];
